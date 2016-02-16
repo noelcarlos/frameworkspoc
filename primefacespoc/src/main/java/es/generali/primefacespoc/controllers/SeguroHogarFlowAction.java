8aeb522363d84084f9c746c1517138c26b1d617f
Noel <nchernandez.perez@atsistemas.com>
2016-02-16 13:35:15 +0100

    Flow controlled navigation

--- primefacespoc/src/main/java/es/generali/primefacespoc/controllers/SeguroHogarFlowAction.java
+++ primefacespoc/src/main/java/es/generali/primefacespoc/controllers/SeguroHogarFlowAction.java
@@ -9,12 +9,15 @@ import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Node;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.binding.message.MessageContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.webflow.execution.RequestContext;
 
 import es.generali.primefacespoc.models.ConfiguracionBean;
 import es.generali.primefacespoc.models.SeguroViviendaBean;
+import es.generali.primefacespoc.models.SeguroViviendaBean.CaracteristicasDeLaVivienda;
+import es.generali.primefacespoc.support.ControlledExit;
 import es.generali.primefacespoc.support.GeneratorHelper;
 
 public class SeguroHogarFlowAction extends StrutsFlowAction {
@@ -50,30 +53,50 @@ public class SeguroHogarFlowAction extends StrutsFlowAction {
 		Node node = flow.selectSingleNode("//flow/step[@name='" + currentStep + "']");
 		currentPageNumber = currentStep;
 		currentPageTitle = node.valueOf("@title");
+		
+		Integer _flowToView = (Integer)requestContext.getFlowScope().get("_flowToView");
+		
+		if (_flowToView != null) {
+			if (_flowToView != currentStep) {
+				throw new ControlledExit("flow to:" + _flowToView);
+			} else {
+				requestContext.getFlowScope().remove("_flowToView");
+			}
+		}
+		System.out.println("Starting on:" + node.valueOf("@view"));
 		return node.valueOf("@view");
 	}
 	
 	public String onStep(RequestContext requestContext, String flowEvent) throws Exception {
+		MessageContext messageContext = requestContext.getMessageContext();
 		Node node = flow.selectSingleNode("//flow");
-		
-		if (flowEvent.equals("gobackward-first")) {
-			currentStep = 1;
-		} else 	if (flowEvent.equals("goforward-next")) {
-			if (currentStep < lastPageNumber) {
+		Integer _flowToView = (Integer)requestContext.getFlowScope().get("_flowToView");
+
+		if (flowEvent != null && !messageContext.hasErrorMessages()) {
+			if (flowEvent.equals("gobackward-first")) {
+				currentStep = 1;
+			} else 	if (flowEvent.equals("goforward-next") || _flowToView != null) {
+				if (currentStep < lastPageNumber) {
+					currentStep++;
+				}
+			} else 	if (flowEvent.equals("gobackward-previous")) {
+				if (currentStep > 1) { 
+					currentStep--;
+				}
+			} else 	if (flowEvent.equals("goforward-last")) {
+				currentStep = lastPageNumber;
+			} else if (flowEvent.startsWith("goforward-")) { 
+				int nextStep = Integer.parseInt(flowEvent.substring(10));
+				if (nextStep - currentStep > 1) {
+					requestContext.getFlowScope().put("_flowToView", nextStep);
+				}
 				currentStep++;
+			} else if (flowEvent.startsWith("gobackward-")) { 
+				int nextStep = Integer.parseInt(flowEvent.substring(11));
+				currentStep = nextStep;
 			}
-		} else 	if (flowEvent.equals("gobackward-previous")) {
-			if (currentStep > 1) { 
-				currentStep--;
-			}
-		} else 	if (flowEvent.equals("goforward-last")) {
-			currentStep = lastPageNumber;
-		} else if (flowEvent.startsWith("goforward-")) { 
-			int nextStep = Integer.parseInt(flowEvent.substring(10));
-			currentStep = nextStep;
-		} else if (flowEvent.startsWith("gobackward-")) { 
-			int nextStep = Integer.parseInt(flowEvent.substring(11));
-			currentStep = nextStep;
+		} else {
+			requestContext.getFlowScope().remove("_flowToView");
 		}
 		
 		node = flow.selectSingleNode("//flow/step[@name='" + currentStep + "']");
@@ -82,6 +105,7 @@ public class SeguroHogarFlowAction extends StrutsFlowAction {
 		
 		node = flow.selectSingleNode("//flow/step[@name='" + currentStep + "']");
 		
+		System.out.println("Next view:" + node.valueOf("@view"));
 		requestContext.getViewScope().put("nextView", node.valueOf("@view"));
 		
 		return null; /*node.valueOf("@view");*/
